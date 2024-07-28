25af0155c30f1fcc46849773e936ebd2219239f8
arushig11 <arushi.gupta2024@gmail.com>
2024-07-27 20:06:33 -0400

    Server side changes

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import * as dotenv from 'dotenv'; // allow us to use environment variables
+import bodyParser from 'body-parser';
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/posts.js';
+import cors from 'cors';
+
+dotenv.config();
+// Initialize the app 
+const app = express()
+
+app.use('/posts',postRoutes);
+
+// Setup Body Parser to send our requests
+app.use(bodyParser.json({limit:"30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
+app.use(cors());
+const startServer = async() => {
+    try{
+        const PORT = process.env.PORT || 8080;
+// Connect server applicaton with database. We are using Mongodb (Atlas version(they host our db on their server))
+// garushi567 4lQpkFlmslKnv00y
+        connectDB(process.env.CONNECTION_URL);
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+startServer();
+
+
+
---
+++ server/mongodb/connect.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'; //modeling library for mongodb to make madels easily
+
+const connectDB = (CONNECTION_URL) => {
+
+    // mongoose to connect to our database
+    mongoose.connect(CONNECTION_URL) 
+        .then(() => console.log('MongoDB conneted')) // if our connection is successful it listens
+        .catch((error) => console.log(error.message));
+
+    mongoose.set('strictQuery', false);
+}
+
+export default connectDB;
